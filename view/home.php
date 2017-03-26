92579783b0f6258357dcbdc81439601afb26deb7
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-03-26 06:52:02 +0200

    Design de la page

--- view/home.php
+++ view/home.php
@@ -3,19 +3,20 @@
     <head>
         <meta charset="utf-8">
         <title>TD1 - Projet Technologique</title>
-    	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css" />
+    	<link rel="stylesheet" type="text/css" href="dist/css/myStyle.css"/>
+    	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css"/>
     </head>
     <body>
         <form class="form" action="signin.php" method="post">
-            <button type="submit" name="signin">Connexion</button>
+            <button type="submit" name="signin" class="btn btn-default btn-lg">Connexion</button>
         </form>
 
         <form class="form" action="signup.php" method="post">
-            <button type="submit" name="signup">Inscription</button>
+            <button type="submit" name="signup" class="btn btn-default btn-lg">Inscription</button>
         </form>
 
         <script type="text/javascript" src="dist/js/myScript.js"></script>
-        <script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="dist/js/jquery-3.2.0.min.js"></script>
+        <script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
     </body>
 </html>
--- view/signup.php
+++ view/signup.php
@@ -3,29 +3,83 @@
     <head>
         <meta charset="utf-8">
         <title>TD1 - Projet Technologique</title>
-    	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css" />
+    	<link rel="stylesheet" type="text/css" href="dist/css/myStyle.css"/>
+    	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css"/>
     </head>
     <body>
-        <form class="form" action="../controller/signup.php" method="post">
-            <label for="">Nom:</label>
-            <input type="text" name="nom" value="" placeholder="Entrer votre nom" required><br><br>
-            <label for="">Prénom:</label>
-            <input type="text" name="prenom" value="" placeholder="Entrer votre prénom" required><br><br>
-            <label for="">Date de naissance:</label>
-            <input type="date" name="dateNaiss" value="" required><br><br>
-            <label for="">Téléphone:</label>
-            <input type="num" id="telPerso" name="telPerso" value="" placeholder="Numéro de téléphone" required><br><br>
-            <label for="">Login:</label>
-            <input type="text" id="login" name="login" placeholder="Donner votre login" required><br><br>
-            <label for="">Mot de passe:</label>
-            <input type="password" id="password" name="password" placeholder="**************" required><br><br>
-            <label for="">Confirmation du mot de passe:</label>
-            <input type="password" id="confirmPassword" name="confirmPassword" placeholder="**************"><br><br>
-            <button type="submit" name="inscription">Inscription</button>
-        </form>
+        <div class="container navigation">
+            <div class="row navigation">
+               <nav class="navbar navbar-default navbar-fixed-top">
+                   <ol class="breadcrumb">
+                       <li><a href="#"><img class="logo"></a></li> <!-- On peut ajouter l'image via le background du CSS -->
+                   </ol>
+                   <div class="navbar-collapse collapse navbar-right">
+                       <ul class="nav navbar-nav">
+
+                       </ul>
+                   </div>
+               </nav>
+           </div> <!-- row navigation -->
+        </div> <!-- container navigation -->
+
+        <div class="container header">
+            <div class="page-header">
+                <h1>Inscription à la plateforme de la Mairie</h1>
+            </div>
+        </div> <!-- container header -->
+
+        <div class="container content">
+            <form class="form-horizontal" action="#" method="post">
+                <!--<div class="form-group form-group-lg">
+                    <label for="" class="control-label control-label-lg">Nom:</label>
+                    <input type="text" class="form-control" name="nom" value="" placeholder="Entrer votre nom" required>
+                </div>
+                <div class="form-group form-group-lg">
+                    <label for="" class="control-label label-lg">Prénom:</label>
+                    <input type="text" class="form-control" name="prenom" value="" placeholder="Entrer votre prénom" required>
+                </div>
+                <div class="form-group form-group-lg">
+                    <label for="" class="control-label label-lg">E-mail:</label>
+                    <input type="email" class="form-control" name="email" value="" placeholder="Entrer votre e-mail" required>
+                </div>
+                <div class="form-group form-group-lg">
+                    <label for="" class="control-label label-lg">Date de naissance:</label>
+                    <input type="date" class="form-control" name="dateNaiss" value="" required>
+                </div>
+                <div class="form-group form-group-lg">
+                    <label for="" class="control-label label-lg">Téléphone:</label>
+                    <input type="num" class="form-control" id="telPerso" name="telPerso" value="" placeholder="Numéro de téléphone" required>
+                </div>
+                <div class="form-group form-group-lg">
+                    <label for="" class="control-label label-lg">Identifiant:</label>
+                    <input type="text" class="form-control" id="login" name="login" placeholder="Donner votre login" required>
+                </div>-->
+                <div class="form-group form-group-lg">
+                    <label for="" class="control-label label-lg">Mot de passe:</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="**************" required>
+                </div>
+                <div class="form-group form-group-lg" id="confirmPasswordDiv">
+                    <label for="" class="control-label label-lg">Confirmation du mot de passe:</label>
+                    <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" onblur="validatePassword()"  placeholder="**************">
+                    <span class="help-block" id="helpConfirmPassword"></span>
+                </div>
+                <div class="form-group form-group-lg">
+                    <div class="col-sm-offset-2 ">
+                        <center><button type="submit" class="btn btn-default btn-lg" name="inscription" >Inscription</button></center>
+                    </div>
+                </div>
+            </form>
+        </div> <!-- container content -->
+
+        <div class="container footer">
+
+        </div> <!-- container footer -->
 
         <script type="text/javascript" src="dist/js/myScript.js"></script>
-        <script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="dist/js/jquery-3.2.0.min.js"></script>
+        <script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+
+        </script>
     </body>
 </html>
