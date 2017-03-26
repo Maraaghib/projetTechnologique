9704b2c5792c19a3a712190b775e1acbdff71a85
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-03-26 08:48:46 +0200

    Juste un petit commentaire

--- model/signup.php
+++ model/signup.php
@@ -3,7 +3,7 @@
 
    	include_once('data/User.php');
    	include_once('data/UserBroker.php');
-	var_dump($user);
+	var_dump($user); // Error: Undifined variable
    	$account->addUser($nom, $prenom, $email, $dateNaiss, $telPerso, $login, $password);
    	var_dump($account);
 
