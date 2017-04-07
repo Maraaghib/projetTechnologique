bcf5644fd9d2cfb77f33f3b57cabb7f9d40fd532
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-08 01:11:52 +0200

    Tester d'abord est ce que le formulaire de connexion est sunmitted

--- controller/signin.php
+++ controller/signin.php
@@ -4,11 +4,13 @@
    session_start();
    $account = $_SESSION['account'];
 
-   $login = $_POST['login'];
-   $password = $_POST['password'];
+   if (isset($_POST['btnLogIn'])) {
+       $login = $_POST['login'];
+       $password = $_POST['password'];
+   }
 
    include('../model/signin.php');
    header('Location: ../view/account.php');
-   
+
 
 ?>
