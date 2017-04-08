b886fc378f443a1bf5b98e6cf77fe5a9ce3f1923
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-08 17:30:16 +0200

    Responsive du formulaire pour message

--- view/account/pages/forum.php
+++ view/account/pages/forum.php
@@ -100,13 +100,13 @@ if (isset($_POST['btnComment'])) {
 <?php endif ?>
 <form class="form" action="#comment" id="comment" method="post" style="margin-bottom: 20px;">
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-md-6 col-xs-12">
             <div class="form-group">
                 <label for="">Pseudo</label>
                 <input type="text" name="username" class="form-control" value="">
             </div>
         </div>
-        <div class="col-xs-6">
+        <div class="col-md-6 col-xs-12">
             <div class="form-group">
                 <label for="">Email</label>
                 <input type="email" name="email" class="form-control" value="">
