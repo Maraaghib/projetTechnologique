4fde704f3933e4d3eecc5f1668fe8f01484a9bc4
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-11 18:36:27 +0200

    Delete unnecessary lines

--- model/signup.php
+++ model/signup.php
@@ -3,8 +3,6 @@
 
    	include_once('data/User.php');
    	include_once('data/UserBroker.php');
-    echo "Je suis sur Model<br>";
    	$isAdded = $account->addUser($nom, $prenom, $email, $dateNaiss, $telPerso, $login, $password);
-    echo "Je suis sur Model 2 <br>";
 
 ?>
