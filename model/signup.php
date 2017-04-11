478227fd24c48f133a5896b5ab5a2b0103469cee
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-11 21:37:45 +0200

    Change the args of addUser function

--- model/signup.php
+++ model/signup.php
@@ -3,6 +3,6 @@
 
    	include_once('data/User.php');
    	include_once('data/UserBroker.php');
-   	$isAdded = $account->addUser($nom, $prenom, $email, $dateNaiss, $telPerso, $login, $password);
+   	$isAdded = $account->addUser($user);
 
 ?>
