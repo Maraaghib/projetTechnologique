b35ebb46aab4b8871e29aa74617778c47e9ef6be
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-11 21:30:59 +0200

    Create a new instance of User class with arguments

--- controller/signup.php
+++ controller/signup.php
@@ -1,25 +1,28 @@
 <?php
 
-   include_once('../model/data/UserBroker.php');
+    include_once('../model/data/UserBroker.php');
 
-   session_start();
+    session_start();
 
-   $account = $_SESSION['account'];
+    $account = $_SESSION['account'];
 
-   $nom = $_POST['nom'];
-   $prenom = $_POST['prenom'];
-   $email = $_POST['email'];
-   $dateNaiss = $_POST['dateNaiss'];
-   $telPerso = $_POST['telPerso'];
-   $login = $_POST['login'];
-   $password = sha1($_POST['password']);
-   include('../model/signup.php');
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $dateNaiss = $_POST['dateNaiss'];
+    $telPerso = $_POST['telPerso'];
+    $login = $_POST['login'];
+    $password = sha1($_POST['password']);
 
-   if ($isAdded) {
+    $user = User::newUser($nom, $prenom, $email, $dateNaiss, $telPerso, $login, $password); // Crée une nouvelle instance de User avec des paramètres
+
+    include('../model/signup.php');
+
+    if ($isAdded) {
        header('Location: ../view/account/');
-   }
-   else {
+    }
+    else {
        echo "L'utilisateur n'a pas été ajouté !<br>";
-   }
+    }
 
 ?>
